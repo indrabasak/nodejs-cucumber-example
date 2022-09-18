3558ea0e78e8b8eb5345abeec3752c58ba2cb510
basaki <indra.basak@autodesk.com>
2022-09-18 13:53:18 -0700

    created a REST service

---
+++ service/src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const NodeCache = require('node-cache');
+const uuid = require('uuid');
+
+const app = express();
+app.use(express.json());
+const cache = new NodeCache({ stdTTL: 15 });
+
+app.post('/books', (req, res) => {
+  const id = uuid.v4().toString();
+  console.log(id);
+  const book = {
+    id,
+    name: req.body.name
+  };
+  cache.set(id, book);
+
+  return res.status(200).json(book);
+});
+
+app.get('/books/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    if (cache.has(id)) {
+      return res.status(200).json(cache.get(id));
+    }
+  } catch (e) {
+    throw new Error(e);
+  }
+
+  return res.status(404).send('Not found');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
+
+const server = app.listen(8081, () => {
+  const host = server.address().address;
+  const { port } = server.address();
+  console.log('Example app listening at http://%s:%s', host, port);
+});
